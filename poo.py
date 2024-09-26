7ebdc46dbf6d95dc1a2bc812d6454f92279659ca
DarcioRafael <liminhadarcioo@gmail.com>
2024-09-25 21:33:39 -0300

    Add files via upload

---
+++ poo.py
@@ -0,0 +1,10 @@
+
+
+print(' O que é python ?')
+print('É uma linguagem de programação de alto nivel !')
+
+print('Qual programa IDE exeulta o arquivo .py ?')
+print('jython, Vs Code e cpython !')
+
+print(' Qual linguagem de progamação python substituiu ?')
+print(' A linguagem ABC !')
